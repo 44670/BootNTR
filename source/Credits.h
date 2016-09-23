f4318fe9aa90da96babc794c1e1d7c78dcb0463e
Nanquitas <nath.doidi+github@gmail.com>
2016-09-23 12:29:59 +0200

    Forgot to add the new files

---
+++ source/Credits.h
@@ -0,0 +1,24 @@
+#ifndef CREDITS_H
+#define CREDITS_H
+
+static const int	creditsCount = 13;
+static const char * credits[] =
+{
+	"AstronautLevel",
+	"AuroraWright",
+	"b1l1s",
+	"Cell9",
+	"d3m3vilurr",
+	"enler",
+	"fincs",
+	"Flat",
+	"FONZD",
+	"lucaboy",
+	"Nanquitas",
+	"PabloMK7",
+	"Steveice10",
+	"TuxSH",
+	"xerpi"
+};
+
+#endif
